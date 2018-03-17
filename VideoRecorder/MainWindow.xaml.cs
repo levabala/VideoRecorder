b3a2f65bc58227f4d2d4c5465a9ddeb5357b1d01
Levabala <levabala@ya.ru>
2018-03-17 18:41:32 +0300

    Initial commit

---
+++ VideoRecorder/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoRecorder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, RecordAction> schedule = new Dictionary<string, RecordAction>()
+        {
+            { "18:10", RecordAction.StartRecording }
+        };
+
+        Recorder rec = new Recorder();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ButtonStartRecording.Click += (a, b) =>
+            {
+                rec.StartRecording();
+            };
+            ButtonStopRecording.Click += (a, b) =>
+            {
+                rec.StopRecording();
+            };
+
+            rec.ListenForSchedule(schedule);        
+
+            rec.captureDevice.NewFrame += CaptureDevice_NewFrame;
+        }
+        
+        private void CaptureDevice_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
+        {
+            Bitmap frame = eventArgs.Frame;
+            ui(() =>
+            {
+                ImageRealTime.Source = BitmapToImageSource(frame);
+            });
+        }
+
+        public void ui(Action act)
+        {
+            try
+            {
+                Dispatcher.Invoke(act);
+            }
+            catch(Exception e)
+            {
+
+            }
+        }
+
+        BitmapImage BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+    }
+}
---
+++ VideoRecorder/Recorder.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Accord.Video.FFMPEG;
+using Accord.Video.VFW;
+using System.Timers;
+
+namespace VideoRecorder
+{   
+    class Recorder
+    {
+        public VideoCaptureDevice captureDevice;
+        VideoFileWriter fileWriter = new VideoFileWriter();
+        VideoCaptureDeviceForm captureDeviceForm = new VideoCaptureDeviceForm();
+        Dictionary<string, RecordAction> schedule = new Dictionary<string, RecordAction>();
+        Action startRecordingAction, stopRecordingAction;
+        Dictionary<RecordAction, Action> actionMap;
+
+        public Recorder()
+        {
+            startRecordingAction = StartRecording;
+            stopRecordingAction = StopRecording;
+            actionMap = new Dictionary<RecordAction, Action>()
+            {
+                { RecordAction.StartRecording, startRecordingAction },
+                { RecordAction.StopRecording, stopRecordingAction }
+            };
+
+            FilterInfoCollection devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);            
+            captureDevice = new VideoCaptureDevice(devices[0].MonikerString);            
+            captureDevice.NewFrame += CaptureDevice_NewFrame;            
+        }
+
+        bool writerSet = false;
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap frame = eventArgs.Frame;
+            if (writerSet)
+            {
+                fileWriter.WriteVideoFrame(frame);
+                return;
+            }
+
+            DateTime dateTime = DateTime.Now;
+            int width = frame.Width;
+            int height = frame.Height;
+            fileWriter.Open(dateTime.Millisecond.ToString() + ".avi", width, height);
+
+            writerSet = true;
+        }
+        
+        public void StartRecording()
+        {            
+            captureDevice.Start();            
+        }
+
+        public void StopRecording()
+        {
+            captureDevice.Stop();
+            fileWriter.Close();
+        }
+
+        bool listening = false;
+        public void ListenForSchedule(Dictionary<string, RecordAction> schedule)
+        {
+            listening = true;
+            this.schedule = schedule;
+            Timer t = new Timer(10000);
+            t.Elapsed += T_Elapsed;
+            t.Start();
+        }
+
+        private void T_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            string key = dt.Hour + ":" + dt.Minute;
+            if (schedule.ContainsKey(key))
+                actionMap[schedule[key]]();
+        }
+    }
+
+    public enum RecordAction
+    {
+        StartRecording,
+        StopRecording
+    }
+}
